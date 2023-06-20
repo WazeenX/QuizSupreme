61d386209d87d695ffffc124945bc06760aac2da
WazeenX <wazeen999@gmail.com>
2023-06-20 14:05:28 +0530

    Quiz terminates and goes to result activity

--- app/src/main/java/com/example/quiz1/ScreenshotProtectionApplication.java
+++ app/src/main/java/com/example/quiz1/ScreenshotProtectionApplication.java
@@ -2,12 +2,14 @@ package com.example.quiz1;
 
 import android.app.Activity;
 import android.app.Application;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.WindowManager;
 
 public class ScreenshotProtectionApplication extends Application {
 
     private boolean shouldResetQuiz = false;
+    private boolean shouldReturnToHomepage = false;
 
     @Override
     public void onCreate() {
@@ -26,8 +28,8 @@ public class ScreenshotProtectionApplication extends Application {
                     WindowManager.LayoutParams.FLAG_SECURE
             );
 
-            // Check if the activity is BasicQuiz, DifficultQuiz, or ResultActivity
-            if (activity instanceof BasicQuiz || activity instanceof DifficultQuiz || activity instanceof ResultActivity) {
+            // Check if the activity is BasicQuiz, DifficultQuiz
+            if (activity instanceof BasicQuiz || activity instanceof DifficultQuiz ) {
                 isQuizActivity = true;
             } else {
                 isQuizActivity = false;
@@ -47,6 +49,8 @@ public class ScreenshotProtectionApplication extends Application {
             // Check if the activity being paused is a quiz activity
             if (isQuizActivity) {
                 ((ScreenshotProtectionApplication) activity.getApplication()).shouldResetQuiz = true;
+            } else if (activity instanceof ResultActivity) {
+                ((ScreenshotProtectionApplication) activity.getApplication()).shouldReturnToHomepage = true;
             }
         }
 
@@ -54,8 +58,17 @@ public class ScreenshotProtectionApplication extends Application {
         public void onActivityStopped(Activity activity) {
             ScreenshotProtectionApplication app = (ScreenshotProtectionApplication) activity.getApplication();
             if (app.shouldResetQuiz) {
-                // Reset the flag to false, do nothing
+                // Reset the flag to false and start ResultActivity
                 app.shouldResetQuiz = false;
+                Intent intent = new Intent(activity, ResultActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                activity.startActivity(intent);
+            } else if (app.shouldReturnToHomepage) {
+                // Reset the flag to false and start MainActivity
+                app.shouldReturnToHomepage = false;
+                Intent intent = new Intent(activity, MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                activity.startActivity(intent);
             }
         }
 
